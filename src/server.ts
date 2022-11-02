272de331790219bdbced7bb43ece11d8bcdb5b32
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-02 11:03:52 -0300

    add pools, guesses & users routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 import type { FastifyInstance } from 'fastify';
 
@@ -19,6 +21,37 @@ async function bootstrap() {
 		const count = await prisma.pool.count();
 
 		return { count };
+	});
+
+	fastify.get('/users/count', async () => {
+		const count = await prisma.user.count();
+
+		return { count };
+	});
+
+	fastify.get('/guesses/count', async () => {
+		const count = await prisma.guess.count();
+
+		return { count };
+	});
+
+	fastify.post('/pools', async (req, reply) => {
+		const createPoolBody = z.object({
+			title: z.string(),
+		});
+		const generateId = new ShortUniqueId({ length: 6 });
+
+		const { title } = createPoolBody.parse(req.body);
+		const code = String(generateId()).toUpperCase();
+
+		await prisma.pool.create({
+			data: {
+				title,
+				code,
+			}
+		})
+
+		return reply.status(201).send({ code });
 	});
 
 	fastify.listen({ port: 3333 });
