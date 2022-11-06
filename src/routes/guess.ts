3e7183c544ee4b530ebd792ebd7f55a3cfa0ee07
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-05 23:37:29 -0300

    add create guess in a game on a game

--- src/routes/guess.ts
+++ src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod";
 import { prisma } from "../libs/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export const guessRoutes = async (fastify: FastifyInstance) => {
 	fastify.get('/guesses/count', async () => {
@@ -8,5 +10,69 @@ export const guessRoutes = async (fastify: FastifyInstance) => {
 		return { count };
 	});
 
+	fastify.post("/pools/:poolId/games/:gameId/guesses", { onRequest: [authenticate] }, async (req, reply) => {
+		const createGuessParams = z.object({
+			poolId: z.string(),
+			gameId: z.string(),
+		});
 
+		const createGuessBody = z.object({
+			firstTeamPoints: z.number(),
+			secondTeamPoints: z.number(),
+		});
+
+		const { poolId, gameId } = createGuessParams.parse(req.params);
+		const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(req.body);
+
+		const participant = await prisma.participant.findUnique({
+			where: {
+				userId_poolId: {
+					poolId: poolId,
+					userId: req.user.sub,
+				}
+			}
+		});
+
+		if (!participant) {
+			return reply.status(400).send({ message: "You are not allowed to create a guess inside this pool" });
+		}
+
+		const guess = await prisma.guess.findUnique({
+			where: {
+				participantId_gameId: {
+					participantId: participant.id,
+					gameId,
+				}
+			}
+		});
+
+		if (guess) {
+			return reply.status(400).send({ message: "You already sent a guess to this game on this pool" });
+		}
+
+		const game = await prisma.game.findUnique({
+			where: {
+				id: gameId,
+			}
+		});
+
+		if (!game) {
+			return reply.status(404).send({ message: "Game not found" });
+		}
+
+		if (game.date < new Date()) {
+			return reply.status(400).send({ message: "You cannot send guesses after the game date" });
+		}
+
+		await prisma.guess.create({
+			data: {
+				gameId,
+				participantId: participant.id,
+				firstTeamPoints,
+				secondTeamPoints,
+			}
+		});
+
+		return reply.status(201).send();
+	});
 }
