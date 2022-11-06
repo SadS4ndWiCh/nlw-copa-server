3f77c12b8d1a8500b07697c32fe691ea65d2cb34
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-05 23:30:15 -0300

    registry fastify jwt

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import type { FastifyInstance } from 'fastify';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 
 import { authRoutes } from './routes/auth';
 import { gameRoutes } from './routes/game';
@@ -14,6 +15,9 @@ async function bootstrap() {
 	});
 
 	fastify.register(cors, { origin: true });
+	fastify.register(jwt, { 
+		secret: process.env.JWT_SECRET_KEY as string,
+	});
 
 	fastify.register(authRoutes);
 	fastify.register(gameRoutes);
